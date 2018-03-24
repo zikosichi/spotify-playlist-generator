337dea76a8cd94c39e4fcc9c09a0ad812064d778
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-03-24 15:29:35 +0100

    refactor: scss color variables

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import spotifyApi from 'utils/spotify'
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>TicketSwap assessment</h1>
-                <p>Good luck! 👍</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>TicketSwap assessment</h1>
+        <p>Good luck! 👍</p>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/js/utils/spotify.js
+++ src/js/utils/spotify.js
@@ -5,22 +5,22 @@ import { client_id, callback_url, auth_url } from 'config/spotify'
 let access_token = Cookies.get('access_token')
 
 if (!window.location.hash && typeof access_token === 'undefined') {
-    const api_url = `${auth_url}?client_id=${client_id}&response_type=token&redirect_uri=${callback_url}`
+  const api_url = `${auth_url}?client_id=${client_id}&response_type=token&redirect_uri=${callback_url}`
 
-    window.location.replace(api_url)
+  window.location.replace(api_url)
 }
 
 if (window.location.hash) {
-    const response = JSON.parse(`{"${window.location.hash.slice(1).replace(/&/g, '", "').replace(/=/g, '" : "')}"}`)
-    const expires = new Date(Date.now() + (parseInt(response.expires_in) * 1000))
+  const response = JSON.parse(`{"${window.location.hash.slice(1).replace(/&/g, '", "').replace(/=/g, '" : "')}"}`)
+  const expires = new Date(Date.now() + (parseInt(response.expires_in) * 1000))
 
-    access_token = response.access_token
+  access_token = response.access_token
 
-    Cookies.set('access_token', access_token, {
-        expires
-    })
+  Cookies.set('access_token', access_token, {
+    expires
+  })
 
-    window.history.replaceState({}, document.title, '/')
+  window.history.replaceState({}, document.title, '/')
 }
 
 const spotifyApi = new SpotifyApi()
