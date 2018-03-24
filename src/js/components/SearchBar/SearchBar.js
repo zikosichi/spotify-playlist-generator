4f7a994d7756cc045c44a52c40e7fdae51b9ce70
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-03-24 16:29:36 +0100

    feat: build search bar layout

--- src/js/components/SearchBar/SearchBar.js
+++ src/js/components/SearchBar/SearchBar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import searchIcon from '../../../assets/icons/search-icon.svg';
+
+// Styles
+import './search-bar.scss';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -6,7 +10,21 @@ class SearchBar extends React.Component {
   }
   render() {
     return (
-      <div>Hello</div>
+      <form className="d-flex search-bar">
+        <div className="input-group search-bar__input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text">
+              <img src={searchIcon}/>
+            </span>
+          </div>
+          <input type="text"
+            className="form-control search-bar__input" />
+        </div>
+        <button type="submit"
+          className="btn btn-success ml-3 search-bar__btn">
+          Search
+        </button>
+      </form>
     );
   }
 }
