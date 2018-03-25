9cd5b334ec2a0710e3c5a49f98b26b86a3dac580
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-03-25 01:16:19 +0100

    feat: show placeholder image if missing

--- src/js/components/SearchBox/ResultsList/ResultItem/ResultAvatar/ResultAvatar.js
+++ src/js/components/SearchBox/ResultsList/ResultItem/ResultAvatar/ResultAvatar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import micIcon from '../../../../../../assets/icons/music-player.svg'
 import './result-avatar.scss';
 
 class ResultsAvatar extends React.Component {
   render() {
     return (
       <div className="avatar-box">
-        <img src={this.props.avatarUrl}
+        <img src={this.props.avatarUrl || micIcon}
              className="avatar"/>
       </div>
     )
