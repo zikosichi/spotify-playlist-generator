6a23b5fa46d7cdcbe85f0666aa2071697a55dbc3
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-03-25 21:40:21 +0200

    feat: remove trailing slach from callback url

--- src/js/config/spotify.js
+++ src/js/config/spotify.js
@@ -1,3 +1,3 @@
 export const client_id = 'a7a6362b66f04d5eaeb8f0b4691a8805'
-export const callback_url = decodeURIComponent(window.location.href)
+export const callback_url = window.location.href.replace(/\/$/, '');
 export const auth_url = 'https://accounts.spotify.com/authorize'
