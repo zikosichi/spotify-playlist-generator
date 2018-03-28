d2d49fd7c815f65b72e78f77bf9a7260ac8efb20
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-03-28 21:11:35 +0200

    feat: update reducers

--- src/js/redux/reducer.js
+++ src/js/redux/reducer.js
@@ -8,20 +8,31 @@ const initialState = fromJS({
     { title: 'Artists', type: 'artist' },
     { title: 'Albums', type: 'album' }
   ],
-  items: []
+  items: [],
+  searchString: ''
 })
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+
     case actionTypes.UPDATE_FIELD_VALUE:
       const { fields, value } = action
       return state.setIn(fields, fromJS(value))
+
     case actionTypes.API_CALL_REQUEST:
       return state.set('isFetching', true)
+
     case actionTypes.API_CALL_SUCCESS:
       return state.set('isFetching', false)
+                  .set('items', fromJS(action.payload))
+
     case actionTypes.API_CALL_FAILURE:
       return state.set('isFetching', false)
+
+    case actionTypes.CLEAR_SEARCH:
+      return state.set('searchString', initialState.get('searchString'))
+                  .set('items', initialState.get('items'))
+
     default:
       return state
   }
